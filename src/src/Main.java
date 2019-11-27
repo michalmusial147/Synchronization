7c66dca719f59b856c8373f04dcdd55135e2ae36
michalmusial147 <michalmusial553@gmail.com>
2019-11-27 16:52:17 +0100

    Initial commit

---
+++ src/src/Main.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String input = args[0];
+	    if(input.equals("deadlock")){
+
+        }
+	    else if(input.equals("livelock")){
+
+        }
+        else if(input.equals("starvation")){
+
+        }
+    }
+}
