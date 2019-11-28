d7f15448fb0997b6d045d25b0f793c4bca5dd17d
michalm <michalmusial2@o2.pl>
2019-11-28 22:45:23 +0100

    Initial2

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,17 +1,46 @@
 package src;
-
+import src.Consumer;
 public class Main {
 
     public static void main(String[] args) {
-	    String input = args[0];
-	    if(input.equals("deadlock")){
 
-        }
-	    else if(input.equals("livelock")){
-
-        }
-        else if(input.equals("starvation")){
-
+        if (args[0].equals("deadlock")) {
+            deadlock();
         }
     }
+    static void deadlock(){
+            String resource1 = "Fork";
+            String resource2 = "Knife";
+            Thread Plato = new Thread(new Runnable() {
+                public void run() {
+                    synchronized (resource1) {
+                        System.out.println("Plato: locked: " + resource1);
+                        try {
+                            Thread.sleep(100);
+                        } catch (Exception e) {
+                        }
+                        synchronized (resource2) {
+                            System.out.println("Plato: locked: " + resource2);
+                        }
+                    }
+                }
+            });
+            Thread Socrates = new Thread(new Runnable() {
+                public void run() {
+                    synchronized (resource2) {
+                        System.out.println("Socrates: locked: " + resource2);
+                        try {
+                            Thread.sleep(100);
+                        } catch (Exception e) {
+                        }
+                        synchronized (resource1) {
+                            System.out.println("Socrates: locked: " + resource1);
+                        }
+                    }
+                }
+            });
+            Plato.start();
+            Socrates.start();
+        }
 }
+
